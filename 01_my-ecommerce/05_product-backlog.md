4f6267a12a946f3c8f242a1b2b4145c338f1c585
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-03-16 20:10:12 +0700

    add postsql since 1 to 7

--- 01_my-ecommerce/05_product-backlog.md
+++ 01_my-ecommerce/05_product-backlog.md
@@ -1,1 +1,6 @@
-push
+As a customer, I want to browse products so that I can find items to buy
+As a customer, I want to add products to a cart so that I can prepare an order
+As a customer, I want to place an order so that I can purchase products
+As an admin, I want to add new products so that I can manage the catalog
+As an admin, I want to view orders so that I can fulfill purchases
+As an admin, I want to recieve feedback product from cutsomer
--- 01_my-ecommerce/06_bring-it-to-javascript.js
+++ 01_my-ecommerce/06_bring-it-to-javascript.js
@@ -1,29 +1,27 @@
-let productlist = [""];
-
 const product_A = {
-    productid: 001,
+    productid: 1001,
     name: "Product A",
     price: 10.99,
     description: "This is product A",
     category: "Category 1",
-    stock: 100, 
+    stock: 50, 
 };
 
 const product_B = {
-    productid: 002,
+    productid: 1002,
     name: "Product B",
     price: 15.99,
     description: "This is product B",
     category: "Category 2",
-    stock: 100, 
+    stock: 80, 
 };
 
-const product_B = {
-    productid: 002,
-    name: "Product B",
-    price: 15.99,
-    description: "This is product B",
-    category: "Category 2",
+const product_C = {
+    productid: 1003,
+    name: "Product C",
+    price: 20.99,
+    description: "This is product C",
+    category: "Category 3",
     stock: 100, 
 };
 onsole.log(productlist);
