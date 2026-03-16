973ae144a38fae8d3fb03dcce84d9c44f315be55
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-03-16 19:06:46 +0700

    ad1

---
+++ 01_my-ecommerce/06_bring-it-to-javascript.js
@@ -0,0 +1,51 @@
+let productlist = [""];
+
+const product_A = {
+    productid: 001,
+    name: "Product A",
+    price: 10.99,
+    description: "This is product A",
+    category: "Category 1",
+    stock: 100, 
+};
+
+const product_B = {
+    productid: 002,
+    name: "Product B",
+    price: 15.99,
+    description: "This is product B",
+    category: "Category 2",
+    stock: 100, 
+};
+
+const product_B = {
+    productid: 002,
+    name: "Product B",
+    price: 15.99,
+    description: "This is product B",
+    category: "Category 2",
+    stock: 100, 
+};
+onsole.log(productlist);
+console.log(product_A.name);
+console.log(product_B.name); 
+console.log(product_C.name);
+
+let cart = [{
+
+},{
+
+
+},{
+
+
+    
+}
+
+
+
+];
+
+product_B = {
+    productid: 2,
+    name: "Product B",  
